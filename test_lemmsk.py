ad65cc64112d930f2f45f13e76d3d93210c77d51
mr.Shu <mr@shu.io>
2015-06-15 21:19:12 +0200

    update: Added more tests
    * Added more tests to feel better.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- test_lemmsk.py
+++ test_lemmsk.py
@@ -8,8 +8,10 @@ import unittest
 class TestSimpleLemmatization(unittest.TestCase):
     def test_lemmatization(self):
         self.assertEqual(lemmsk.lemmatize('kuraciemu'), 'kurací')
+        self.assertEqual(lemmsk.lemmatize('kuracieho'), 'kurací')
 
         self.assertTrue(lemmsk.is_lemma('kurací'))
+        self.assertFalse(lemmsk.is_lemma('kuraciemu'))
 
 
 if __name__ == '__main__':
